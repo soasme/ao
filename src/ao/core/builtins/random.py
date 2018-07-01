c14f2f4132a56f9b84433baa3b69456bd30bf5ee
soasme <soasme@gmail.com>
2018-07-01 22:13:12 +1200

    remove core src.

--- src/ao/core/builtins/random.py
+++
@@ -1,5 +0,0 @@
-from rpython.rlib.rrandom import Random
-from ao.core.types import Float
-
-def random(params):
-    return Float(Random().random())
--- src/ao/core/builtins/registry.py
+++
@@ -1,5 +0,0 @@
-from ao.core.builtins.random import random as _random_random_
-
-BUILTIN_REGISTRY = {
-    1: _random_random_,
-}
